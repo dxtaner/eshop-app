2d61a7b6f7dae829f66f977b322c02226f907086
taner <tanerozer16@gmail.com>
2023-11-23 14:31:05 +0300

    Product route add

---
+++ eshop-app-nodej-reactjs/backend-nodejs/routes/product.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../controllers/product/product.js");
+// const cache = require("../cache.js");
+
+const grantAccess = require("../middlewares/grantAccess.js");
+const { verifyAccessToken } = require("../helpers/jwt");
+
+router.post(
+  "/",
+  verifyAccessToken,
+  grantAccess("createAny", "product"),
+  Product.Create
+);
+router.get("/:product_id", Product.Get);
+router.get("/", Product.GetList);
+router.put("/:product_id", Product.Update);
+router.delete("/:product_id", Product.Delete);
+
+module.exports = router;
