60f82dfa1e365aa5a5ef6811f1354a2b8c9ce8aa
taner <tanerozer16@gmail.com>
2023-11-27 15:48:15 +0300

    signin add

---
+++ eshop-app-nodej-reactjs/frontend-react/src/pages/Auth/SignIn/Signin.js
@@ -0,0 +1,90 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Alert,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import validationSchema from "./validations.js";
+import { fetchLogin } from "../../../api";
+import { useAuth } from "../../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function Signin() {
+  const { login } = useAuth();
+  const nav = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values, bag) => {
+      try {
+        const loginResponse = await fetchLogin({
+          email: values.email,
+          password: values.password,
+        });
+
+        login(loginResponse);
+        nav("/profile");
+      } catch (e) {
+        bag.setErrors({ general: e.response.data.message });
+      }
+    },
+  });
+
+  return (
+    <Flex align="center" justify="center" h="100vh">
+      <Box
+        maxW="md"
+        w="full"
+        p={8}
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg">
+        <Heading mb={4}>Sign In</Heading>
+        {formik.errors.general && (
+          <Alert mb={3} status="error">
+            {formik.errors.general}
+          </Alert>
+        )}
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl mb={4}>
+            <FormLabel>Email</FormLabel>
+            <Input
+              name="email"
+              fontWeight={800}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              isInvalid={formik.touched.email && formik.errors.email}
+            />
+          </FormControl>
+          <FormControl mb={4}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              fontWeight={800}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              isInvalid={formik.touched.password && formik.errors.password}
+            />
+          </FormControl>
+          <Button colorScheme="yellow" width="full" type="submit" mt={4}>
+            Sign In
+          </Button>
+        </form>
+      </Box>
+    </Flex>
+  );
+}
+
+export default Signin;
